990fa4bbcd58a5b74f9e1445b906c44687d835be
Geotiger123 <82854175+Geotiger123@users.noreply.github.com>
2021-05-19 10:00:33 -0700

    Delete src directory

--- src/main/java/com/vaadin/tutorial/crm/backend/repository/IngredientsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vaadin.tutorial.crm.backend.repository;
-
-import com.vaadin.tutorial.crm.backend.entity.ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientsRepository extends JpaRepository<ingredient,Long> {
-}
--- src/main/java/com/vaadin/tutorial/crm/backend/repository/RecipesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vaadin.tutorial.crm.backend.repository;
-
-import com.vaadin.tutorial.crm.backend.entity.recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipesRepository extends JpaRepository<recipe,Long> {
-}
